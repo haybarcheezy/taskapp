386e2e3fcb164d42ffedf3232c068fd4eef105d6
Hayden <30369975+haybarcheezy@users.noreply.github.com>
2020-02-18 17:34:34 -0600

    Page Organization

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let ourList = document.querySelector("#our-list")
 let ourForm = document.querySelector("#our-form")
 
 function createItem(x) {
-    ourHTML = `<li class="newlist" style="border-color: #dbdbdb">${x} <button onclick="deleteIt(this)">delete</button></li>`
+    ourHTML = `<li class="newlist" style="border-color: #dbdbdb">${x} <button onclick="deleteIt(this)" class="deletebutton">delete</button></li>`
     ourList.insertAdjacentHTML("beforeend", ourHTML)
     ourInput.value = ""
     ourInput.focus()
